6e61f3e18b593696a1876f2d5eec62e7cee9cc13
Joshua Granick <jgranick@users.noreply.github.com>
2018-01-09 11:33:06 -0800

    Add NyanCat sample

---
+++ demos/NyanCat/src/app.js
@@ -0,0 +1,27 @@
+import Sprite from "openfl/display/Sprite";
+import Stage from "openfl/display/Stage";
+import AssetLibrary from "openfl/utils/AssetLibrary";
+
+
+class App extends Sprite {
+	
+	
+	constructor () {
+		
+		super ();
+		
+		AssetLibrary.loadFromFile ("library.bundle").onComplete ((library) => {
+			
+			var cat = library.getMovieClip ("NyanCatAnimation");
+			this.addChild (cat);
+			
+		}).onError (e => console.error (e));
+		
+	}
+	
+	
+}
+
+
+var stage = new Stage (550, 400, 0xFFFFFF, App);
+document.body.appendChild (stage.element);
