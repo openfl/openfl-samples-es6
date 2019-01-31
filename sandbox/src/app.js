e580a6c7ae4d30bbd2950c316e27c16d315c21c1
Joshua Granick <jgranick@users.noreply.github.com>
2019-01-31 10:42:49 -0800

    Update sandbox template

--- sandbox/src/app.js
+++ sandbox/src/app.js
@@ -1,11 +1,16 @@
-import { Sprite, Stage } from "openfl";
+import { Loader, Sprite, Stage, URLRequest } from "openfl";
+import iconPath from "../icon.png";
 
 class App extends Sprite {
   constructor() {
     super();
-    console.log("Hello World!");
+    var icon = new Loader();
+    icon.load(new URLRequest(iconPath));
+    this.addChild(icon);
   }
 }
 
 var stage = new Stage(550, 400, 0xffffff, App);
-document.getElementById("app").appendChild(stage.element);
+var element = document.getElementById("app");
+while (element.lastChild) element.removeChild(element.lastChild);
+element.appendChild(stage.element);
